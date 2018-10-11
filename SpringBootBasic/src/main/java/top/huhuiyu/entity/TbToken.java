5fd7e60c60affb36b732f9e45494973d4d614cf7
DarkKnightVip <huhuiyu.vip@gmail.com>
2018-10-11 11:20:18 +0800

    mybatis初始化

---
+++ SpringBootBasic/src/main/java/top/huhuiyu/entity/TbToken.java
@@ -0,0 +1,57 @@
+package top.huhuiyu.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * TbToken表
+ * 
+ * @author DarkKnight
+ *
+ */
+public class TbToken implements Serializable {
+
+  private static final long serialVersionUID = -2313018616200398517L;
+  private String token;
+  private Integer uid;
+  private Date lastupdate;
+
+  public TbToken() {
+  }
+
+  public TbToken(String token, Integer uid, Date lastupdate) {
+    this.token = token;
+    this.uid = uid;
+    this.lastupdate = lastupdate;
+  }
+
+  @Override
+  public String toString() {
+    return "TbToken [token=" + token + ", uid=" + uid + ", lastupdate=" + lastupdate + "]";
+  }
+
+  public String getToken() {
+    return token;
+  }
+
+  public void setToken(String token) {
+    this.token = token;
+  }
+
+  public Integer getUid() {
+    return uid;
+  }
+
+  public void setUid(Integer uid) {
+    this.uid = uid;
+  }
+
+  public Date getLastupdate() {
+    return lastupdate;
+  }
+
+  public void setLastupdate(Date lastupdate) {
+    this.lastupdate = lastupdate;
+  }
+
+}
---
+++ SpringBootBasic/src/main/java/top/huhuiyu/entity/TbUser.java
@@ -0,0 +1,88 @@
+package top.huhuiyu.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * TbUser表
+ * 
+ * @author DarkKnight
+ *
+ */
+public class TbUser implements Serializable {
+
+  private static final long serialVersionUID = -5725225942248651055L;
+  private Integer uid;
+  private String username;
+  private String password;
+  private String nickname;
+  private String isEnable;
+  private Date regDate;
+
+  public TbUser() {
+  }
+
+  public TbUser(Integer uid, String username, String password, String nickname, String isEnable, Date regDate) {
+    this.uid = uid;
+    this.username = username;
+    this.password = password;
+    this.nickname = nickname;
+    this.isEnable = isEnable;
+    this.regDate = regDate;
+  }
+
+  @Override
+  public String toString() {
+    return "TbUser [uid=" + uid + ", username=" + username + ", password=" + password + ", nickname=" + nickname
+        + ", isEnable=" + isEnable + ", regDate=" + regDate + "]";
+  }
+
+  public Integer getUid() {
+    return uid;
+  }
+
+  public void setUid(Integer uid) {
+    this.uid = uid;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public void setNickname(String nickname) {
+    this.nickname = nickname;
+  }
+
+  public String getIsEnable() {
+    return isEnable;
+  }
+
+  public void setIsEnable(String isEnable) {
+    this.isEnable = isEnable;
+  }
+
+  public Date getRegDate() {
+    return regDate;
+  }
+
+  public void setRegDate(Date regDate) {
+    this.regDate = regDate;
+  }
+
+}
