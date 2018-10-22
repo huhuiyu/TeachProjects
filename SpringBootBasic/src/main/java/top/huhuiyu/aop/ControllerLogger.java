40ca33320d92ca501bb3fa6ab7f985581b12fcb2
DarkKnightVip <huhuiyu.vip@gmail.com>
2018-10-22 09:28:16 +0800

    切面logger

--- SpringBootBasic/src/main/java/top/huhuiyu/aop/ControllerLogger.java
+++ SpringBootBasic/src/main/java/top/huhuiyu/aop/ControllerLogger.java
@@ -3,7 +3,6 @@ package top.huhuiyu.aop;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
