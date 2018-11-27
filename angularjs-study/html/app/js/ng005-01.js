4e54ac3dd0de3f2358bda88465cbe2b3c724ca1f
huhuiyu <1069306849@qq.com>
2018-11-27 16:16:49 +0800

    checkbox全选,ng-class

--- angularjs-study/html/app/js/ng005-01.js
+++ angularjs-study/html/app/js/ng005-01.js
@@ -14,7 +14,43 @@ angular.element(document).ready(function() {
 
     //业务逻辑
     $scope.welcome = '全选反选演示';
-    
 
+    //多选数据
+    $scope.datas = [
+      { id: 100, info: '电影', title: '就是电影', selected: false },
+      { id: 101, info: '音乐', title: '音乐的描述', selected: false },
+      { id: 102, info: '游戏', title: 'steam,wegame...', selected: false },
+      { id: 103, info: '篮球', title: 'nba2019垃圾年货', selected: false }
+    ];
+
+    //计算属性，显示选中的id列表
+    $scope.selectedIds = function() {
+      var ids = [];
+      for (var i = 0; i < $scope.datas.length; i++) {
+        var d = $scope.datas[i];
+        if (d.selected) {
+          ids.push(d.id);
+        }
+      }
+      return ids.join(',');
+    };
+
+    //全选
+    $scope.checkAll = false;
+
+    $scope.doCheckAll = function() {
+      for (var i = 0; i < $scope.datas.length; i++) {
+        var d = $scope.datas[i];
+        d.selected = $scope.checkAll;
+      }
+    };
+
+    //反选
+    $scope.doCheckRev = function() {
+      for (var i = 0; i < $scope.datas.length; i++) {
+        var d = $scope.datas[i];
+        d.selected = !d.selected;
+      }
+    };
   }
 });
