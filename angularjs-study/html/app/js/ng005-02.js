3b2305e462725a9936c8df55fdcb445134105a3a
huhuiyu <1069306849@qq.com>
2018-11-28 08:44:41 +0800

    单选效果

--- angularjs-study/html/app/js/ng005-02.js
+++ angularjs-study/html/app/js/ng005-02.js
@@ -14,5 +14,25 @@ angular.element(document).ready(function() {
 
     //业务逻辑
     $scope.welcome = '单选按钮演示';
+
+    //性别单选
+    $scope.sex = 'm';
+
+    //动态学历单选
+    $scope.edus = [
+      { eid: 1000, name: '高中' },
+      { eid: 1001, name: '中专' },
+      { eid: 1002, name: '大专' },
+      { eid: 1003, name: '本科' }
+    ];
+
+    //默认选中的学历
+    $scope.edu = $scope.edus[2].eid;
+
+    //更改选中的学历
+    $scope.changeEdu = function(eid) {
+      $log.debug(eid);
+      $scope.edu = eid;
+    };
   }
 });
