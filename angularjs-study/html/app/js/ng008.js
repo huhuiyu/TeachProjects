88ef0fed600838ecbe4cf5762b0ed2bc3cc1cd7f
huhuiyu <1069306849@qq.com>
2018-12-04 10:47:11 +0800

    定时服务

--- angularjs-study/html/app/js/ng008.js
+++ angularjs-study/html/app/js/ng008.js
@@ -11,6 +11,42 @@
         $log.debug('MyCtrl destroy...');
       });
 
+      //延时控制div消失
+      $scope.showDiv = true;
+      //$timeout就是js里面setTimeout，但是$timeout更改变量会体现在页面上
+      //第一个参数是延时执行的function，第二个参数是延时时间
+      //返回值是用于取消延时任务的timer的id
+      var timer = $timeout(function() {
+        $scope.showDiv = false;
+      }, 5000);
+
+      $scope.cancelHideDiv = function() {
+        //通过timer的id取消延时任务,$timeout.cancel(timer的id)
+        $timeout.cancel(timer);
+      };
+
+      //获取当前时间
+      function getNowTime() {
+        var date = new Date();
+        //年份
+        var year = date.getFullYear();
+        //月份
+        var month = date.getMonth() + 1;
+        //日期
+        var day = date.getDate();
+        //时
+        var hour = date.getHours();
+        //分
+        var minute = date.getMinutes();
+        //秒
+        var second = date.getSeconds();
+        var result = year + '年' + month + '月' + day + '日 ';
+        result = result + hour + ':' + minute + ':' + second;
+        return result;
+      }
+
+      $scope.nowtime = getNowTime();
+
       
     }
   });
